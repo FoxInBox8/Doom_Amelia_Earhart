399486c6ed0c1b71336aa229efd48df3426ae98c
Aaron Lurie <aaron.lurie@mymail.champlain.edu>
2023-06-25 22:11:00 -0400

    Enemies now spawn

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -8,7 +8,7 @@ public class WaveManager : MonoBehaviour
     private int startCredits, creditsPerWave;
 
     [SerializeField]
-    private List<IEnemy> crabs;
+    private List<Crab> enemies;
 
     private int currentCredits;
     private EnemySpawnPoint[] spawnPoints;
@@ -34,20 +34,20 @@ public class WaveManager : MonoBehaviour
 
     private void spawnWave()
     {
-        List<IEnemy> affordableCrabs = new(crabs);
+        List<Crab> affordableEnemies = new(enemies);
 
         // Spawning algorithm:
         //  1. Choose a random crab from the list
         //  2. If that crab is too expensive, remove it from the list
         //  3. If we can afford that crab, spawn it and subtract its credit cost
         //  4. Keep doing this until we can't spawn any more enemies
-        while (affordableCrabs.Count > 0)
+        while (affordableEnemies.Count > 0)
         {
-            int randomEnemy = Random.Range(0, affordableCrabs.Count);
+            int randomEnemy = Random.Range(0, affordableEnemies.Count);
 
-            if (affordableCrabs[randomEnemy].GetStats().CreditCost > currentCredits)
+            if (affordableEnemies[randomEnemy].GetStats().CreditCost > currentCredits)
             {
-                affordableCrabs.Remove(affordableCrabs[randomEnemy]);
+                affordableEnemies.Remove(affordableEnemies[randomEnemy]);
             }
 
             else
@@ -56,16 +56,11 @@ public class WaveManager : MonoBehaviour
                 int randomSpawnPoint = Random.Range(0, spawnPoints.Length);
                 Vector3 spawnPos = spawnPoints[randomSpawnPoint].getNearbyPoint();
 
-				var enemy = affordableCrabs[randomEnemy] as MonoBehaviour;
+				// Make sure all enemies spawn on the ground, will probably break if the terrain is not all flat
+				spawnPos.y = spawnPoints[randomSpawnPoint].transform.position.y + affordableEnemies[randomEnemy].transform.localScale.y / 2;
 
-				if (enemy != null)
-				{
-					// Make sure all enemies spawn on the ground, will probably break if the terrain is not all flat
-					spawnPos.y = enemy.transform.localScale.y / 2;
-
-					Instantiate(enemy, spawnPos, transform.rotation);
-					currentCredits -= affordableCrabs[randomEnemy].GetStats().CreditCost;
-				}
+                Instantiate(affordableEnemies[randomEnemy], spawnPos, transform.rotation);
+				currentCredits -= affordableEnemies[randomEnemy].GetStats().CreditCost;
 			}
         }
     }
